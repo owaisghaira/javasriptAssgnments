054418ec3b9b97a75e6e6488ea634eba52787c14
owaisghaira <owais.ghaira99@gmail.com>
2020-06-07 15:55:35 +0500

    javasript assignment 1 done.

---
+++ app.js
@@ -0,0 +1,22 @@
+//  Chapter #1 assignmet
+//1.
+ // alert ("welcome to my pet store");
+
+//2
+//alert ("Error!Please enter a valid password");
+
+//3
+//alert ("Welcome to js Land...  \n happy coding ")
+
+//4
+//alert ("Welcome to js land...")
+//alert ("Happy coding" )
+
+//5
+//console.log ("Hello...I can run Js throug my web browser's console")
+
+//6
+//coding in html
+
+//7
+//coding in html
